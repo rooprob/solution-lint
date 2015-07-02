45b996202f34c78933411d1fdf4d4c9f0ea85486
Robert Fielding <fieldingrp@gmail.com>
2015-07-02 21:29:39 +1200

    Refactor Data class

--- spec/unit/solution-lint-checks_spec.rb
+++ spec/unit/solution-lint-checks_spec.rb
@@ -6,15 +6,15 @@ describe SolutionLint::Checks do
   it 'should have no problems when loading valid file' do
     linter = SolutionLint.new
     linter.file = valid_file_path
-    subject.load_data(linter.path, linter.content)
-    expect(subject.problems.length).to eq(0)
+    problems = subject.load_data(linter.path, linter.content)
+    expect(problems.length).to eq(0)
   end
 
   it 'should have one problem when loading invalid file' do
     linter = SolutionLint.new
     linter.file = fail_file_path
-    subject.load_data(linter.path, linter.content)
-    expect(subject.problems.length).to eq(1)
+    problems = subject.load_data(linter.path, linter.content)
+    expect(problems.length).to eq(1)
   end
 
 end

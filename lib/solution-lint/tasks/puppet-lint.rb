a103d32da4d422fac43fdf890edd47914c549bc2
Robert Fielding <fieldingrp@gmail.com>
2015-07-02 21:39:41 +1200

    Remove superfluous puppet-lint code

--- lib/solution-lint/tasks/puppet-lint.rb
+++ lib/solution-lint/tasks/puppet-lint.rb
@@ -1,19 +1,19 @@
-require 'puppet-lint'
-require 'puppet-lint/optparser'
+require 'solution-lint'
+require 'solution-lint/optparser'
 require 'rake'
 require 'rake/tasklib'
 
-class PuppetLint
+class SolutionLint
   # Public: A Rake task that can be loaded and used with everything you need.
   #
   # Examples
   #
-  #   require 'puppet-lint'
-  #   PuppetLint::RakeTask.new
+  #   require 'solution-lint'
+  #   SolutionLint::RakeTask.new
   class RakeTask < ::Rake::TaskLib
     include ::Rake::DSL if defined?(::Rake::DSL)
 
-    DEFAULT_PATTERN = '**/*.pp'
+    DEFAULT_PATTERN = '**/*.yaml'
 
     attr_accessor :name
     attr_accessor :pattern
@@ -28,13 +28,13 @@ class PuppetLint
     attr_accessor :show_ignored
     attr_accessor :relative
 
-    # Public: Initialise a new PuppetLint::RakeTask.
+    # Public: Initialise a new SolutionLint::RakeTask.
     #
     # args - Not used.
     #
     # Example
     #
-    #   PuppetLint::RakeTask.new
+    #   SolutionLint::RakeTask.new
     def initialize(*args, &task_block)
       @name = args.shift || :lint
       @pattern = DEFAULT_PATTERN
@@ -46,41 +46,41 @@ class PuppetLint
     end
 
     def define(args, &task_block)
-      desc 'Run puppet-lint'
+      desc 'Run solution-lint'
 
       task_block.call(*[self, args].slice(0, task_block.arity)) if task_block
 
       # clear any (auto-)pre-existing task
       Rake::Task[@name].clear if Rake::Task.task_defined?(@name)
       task @name do
-        PuppetLint::OptParser.build
+        SolutionLint::OptParser.build
 
         Array(@disable_checks).each do |check|
-          PuppetLint.configuration.send("disable_#{check}")
+          SolutionLint.configuration.send("disable_#{check}")
         end
 
         %w{with_filename fail_on_warnings error_level log_format with_context fix show_ignored relative}.each do |config|
           value = instance_variable_get("@#{config}")
-          PuppetLint.configuration.send("#{config}=".to_sym, value) unless value.nil?
+          SolutionLint.configuration.send("#{config}=".to_sym, value) unless value.nil?
         end
 
-        if PuppetLint.configuration.ignore_paths
-          @ignore_paths = PuppetLint.configuration.ignore_paths
+        if SolutionLint.configuration.ignore_paths
+          @ignore_paths = SolutionLint.configuration.ignore_paths
         end
 
         RakeFileUtils.send(:verbose, true) do
-          linter = PuppetLint.new
+          linter = SolutionLint.new
           matched_files = FileList[@pattern]
 
           matched_files = matched_files.exclude(*@ignore_paths)
 
-          matched_files.to_a.each do |puppet_file|
-            linter.file = puppet_file
+          matched_files.to_a.each do |solution_file|
+            linter.file = solution_file
             linter.run
             linter.print_problems
           end
           abort if linter.errors? || (
-            linter.warnings? && PuppetLint.configuration.fail_on_warnings
+            linter.warnings? && SolutionLint.configuration.fail_on_warnings
           )
         end
       end
@@ -88,4 +88,4 @@ class PuppetLint
   end
 end
 
-PuppetLint::RakeTask.new
+SolutionLint::RakeTask.new
